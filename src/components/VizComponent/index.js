da0cfda1f1fa0916cd413b328020ec6b64e6aa64
Colin Roy-Ehri <colin.roy.ehri@gmail.com>
2022-01-07 14:47:10 -0700

    vizComponent added

---
+++ src/components/VizComponent/index.js
@@ -0,0 +1,14 @@
+// Not yet implemented in the App.js file
+// To use this functionality, add a Route to this content
+
+import React from 'react'
+import EmbedComponent from './VizComponent'
+
+
+const VizComponent = ((props) => {
+  return (
+    <EmbedComponent {...props} />
+  )
+})
+
+export default VizComponent

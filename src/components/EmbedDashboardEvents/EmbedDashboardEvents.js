39085fc6e9b014bd4f3ad64a02d8803cd4abb507
Colin Roy-Ehri <colin.roy.ehri@gmail.com>
2022-01-24 15:26:10 -0700

    js update events working

--- src/components/EmbedDashboardEvents/EmbedDashboardEvents.js
+++ src/components/EmbedDashboardEvents/EmbedDashboardEvents.js
@@ -9,9 +9,12 @@
 import React, { useCallback } from 'react'
 import styled from "styled-components"
 import { LookerEmbedSDK } from '@looker/embed-sdk'
-import { Space, Link, SpaceVertical } from '@looker/components'
+import { Space, Link, SpaceVertical, Button } from '@looker/components'
 
 const EmbedDashboardEvents = () => {
+
+  // This variable will hold a reference to the dashboard, so we can send messages to it
+  let dashboardRef;
 
   /*
    Step 1 Initialization of the EmbedSDK happens when the user first access the application
@@ -30,28 +33,30 @@ const EmbedDashboardEvents = () => {
         // this instructs the SDK to point to the /dashboards-next/ version
         .withNext()
         // the .on() method allows us to listen for and respond to events inside the iframe. See here for a list of events: https://docs.looker.com/reference/embedding/embed-javascript-events
-        .on('dashboard:loaded', (e) => { alert('Successfully Loaded!') })
-        .on('dashboard:loaded', (e) => { console.log('On dashboards where the tiles are not set to auto-run, a dashboard and its elements have loaded but queries are not yet running.'); console.log(e)})
-        .on('dashboard:run:start', (e) => { console.log('A dashboard has begun loading, and its tiles have started loading and querying for data.'); console.log(e)})
-        .on('dashboard:run:complete', (e) => { console.log('A dashboard has finished running and all tiles have finished loading and querying.'); console.log(e)})
-        .on('dashboard:download', (e) => { console.log('A PDF of a dashboard has started downloading.'); console.log(e)})
-        .on('dashboard:save:complete', (e) => { console.log('A dashboard has been edited and saved.'); console.log(e)})
-        .on('dashboard:delete:complete', (e) => { console.log('A dashboard has been deleted.'); console.log(e)})
-        .on('dashboard:tile:start', (e) => { console.log('A tile has started loading or querying for data.'); console.log(e)})
-        .on('dashboard:tile:complete', (e) => { console.log('A tile has finished running the query.'); console.log(e)})
-        .on('dashboard:tile:download', (e) => { console.log('Tile data has started downloading.'); console.log(e)})
-        .on('dashboard:tile:explore', (e) => { console.log('A user has clicked the Explore From Here option in a dashboard tile.'); console.log(e)})
-        .on('dashboard:tile:view', (e) => { console.log('A user has clicked the View Original Look option in a dashboard tile.'); console.log(e)})
-        .on('dashboard:filters:changed', (e) => { console.log('Filters have been applied or changed.'); console.log(e)})
-        .on('drillmenu:click', (e) => { console.log('A user has clicked on a drill menu in a dashboard that was created with the link LookML parameter.'); console.log(e)})
-        .on('drillmodal:download', (e) => { console.log('ADDED7.20 A user has opened a drill dialog box from a dashboard tile and clicked the Download option.'); console.log(e)})
-        .on('drillmodal:explore', (e) => { console.log('A user has clicked the Explore From Here option in a drill dialog box.'); console.log(e)})
-        .on('page:changed', (e) => { console.log('A user has navigated to a new page within the iframe.'); console.log(e)})
-        .on('page:properties:changed', (e) => { console.log(e,'Page loaded'); console.log(e)})
+        .on('dashboard:loaded', (e) => { console.log('On dashboards where the tiles are not set to auto-run, a dashboard and its elements have loaded but queries are not yet running.'); console.log(e) })
+        .on('dashboard:run:start', (e) => { console.log('A dashboard has begun loading, and its tiles have started loading and querying for data.'); console.log(e) })
+        .on('dashboard:run:complete', (e) => { console.log('A dashboard has finished running and all tiles have finished loading and querying.'); console.log(e) })
+        .on('dashboard:download', (e) => { console.log('A PDF of a dashboard has started downloading.'); console.log(e) })
+        .on('dashboard:save:complete', (e) => { console.log('A dashboard has been edited and saved.'); console.log(e) })
+        .on('dashboard:delete:complete', (e) => { console.log('A dashboard has been deleted.'); console.log(e) })
+        .on('dashboard:tile:start', (e) => { console.log('A tile has started loading or querying for data.'); console.log(e) })
+        .on('dashboard:tile:complete', (e) => { console.log('A tile has finished running the query.'); console.log(e) })
+        .on('dashboard:tile:download', (e) => { console.log('Tile data has started downloading.'); console.log(e) })
+        .on('dashboard:tile:explore', (e) => { console.log('A user has clicked the Explore From Here option in a dashboard tile.'); console.log(e) })
+        .on('dashboard:tile:view', (e) => { console.log('A user has clicked the View Original Look option in a dashboard tile.'); console.log(e) })
+        .on('dashboard:filters:changed', (e) => { console.log('Filters have been applied or changed.'); console.log(e) })
+        .on('drillmenu:click', (e) => { console.log('A user has clicked on a drill menu in a dashboard that was created with the link LookML parameter.'); console.log(e) })
+        .on('drillmodal:download', (e) => { console.log('ADDED7.20 A user has opened a drill dialog box from a dashboard tile and clicked the Download option.'); console.log(e) })
+        .on('drillmodal:explore', (e) => { console.log('A user has clicked the Explore From Here option in a drill dialog box.'); console.log(e) })
+        .on('page:changed', (e) => { console.log('A user has navigated to a new page within the iframe.'); console.log(e) })
+        .on('page:properties:changed', (e) => { console.log(e, 'Page loaded'); console.log(e) })
         // this line performs the call to the auth service to get the iframe's src='' url, places it in the iframe and the client performs the request to Looker
         .build()
         // this establishes event communication between the iframe and parent page
         .connect()
+        // This takes the dashboard's reference and assigns it to a variable defined earlier.
+        // It will be used for sending requests to the dashboard.
+        .then(x => dashboardRef = x)
         // catch various errors which can occur in the process (note: does not catch 404 on content)
         .catch((error) => {
           console.error('An unexpected error occurred', error)
@@ -64,11 +69,19 @@ const EmbedDashboardEvents = () => {
     <Space>
       <div className={"embed-dashboard-main"}>
         <SpaceVertical>
-        <PageTitle >Embedded Dashboard with JavaScript Events</PageTitle>
-        <DevMessage>View the JavaScript event details logged in the <Link href='https://en.wikipedia.org/wiki/Web_development_tools'>developer console.</Link>
-        </DevMessage>
-        { /* Step 0) we have a simple container, which performs a callback to our makeDashboard function */}
-        <Dashboard ref={makeDashboard}></Dashboard>
+          <PageTitle >Embedded Dashboard with JavaScript Events</PageTitle>
+          <DevMessage>View the JavaScript event details logged in the <Link href='https://en.wikipedia.org/wiki/Web_development_tools'>developer console.</Link>
+          </DevMessage>
+          <Space marginLeft={'3rem'}>
+            {/* 
+            These methods are from https://looker-open-source.github.io/embed-sdk/classes/lookerembeddashboard.html 
+            They allow interaction with the Dashboard from the parent page.
+            */}
+            <Button onClick={() => dashboardRef.run()}>Run</Button>
+            <Button onClick={() => dashboardRef.stop()}>Stop</Button>
+          </Space>
+          { /* Step 0) we have a simple container, which performs a callback to our makeDashboard function */}
+          <Dashboard ref={makeDashboard}></Dashboard>
         </SpaceVertical>
       </div>
     </Space>

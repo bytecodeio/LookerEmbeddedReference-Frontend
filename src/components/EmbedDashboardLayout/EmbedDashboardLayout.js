035ad4c1ba379a27328cd80d238e7e14f30666a9
Sean Pagnon <sean.pagnon@bytecode.io>
2022-02-24 10:29:40 -0800

    update dynamic dashboard color toggle to two options

--- src/components/EmbedDashboardLayout/EmbedDashboardLayout.js
+++ src/components/EmbedDashboardLayout/EmbedDashboardLayout.js
@@ -1,38 +1,44 @@
-import React, { useCallback, useEffect }  from 'react'
-import styled from "styled-components"
+import React, { useCallback } from "react";
+import styled from "styled-components";
 //Alias an additional import of the embed sdk
-import { LookerEmbedDashboard, LookerEmbedSDK, LookerEmbedSDK as LookerEmbedSDK2 } from '@looker/embed-sdk'
-import { Button, ButtonItem, ButtonToggle, ToggleSwitch, Space } from '@looker/components'
+import {
+  LookerEmbedSDK,
+  LookerEmbedSDK as LookerEmbedSDK2,
+} from "@looker/embed-sdk";
+import { ButtonItem, ButtonToggle } from "@looker/components";
 
 // Additional js file that holds static values that will be used in this component
-import { newLayoutComponents, bluePallette } from './constant'
-import { PageTitle } from '../common/PageTitle'
+import { newLayoutComponents, bluePallette, defaultPallette } from "./constant";
+import { PageTitle } from "../common/PageTitle";
 
 const EmbedDashboardLayout = () => {
-  const [dashboard,setDashboard] = React.useState()
-  const [dashboardOptions, setDashboardOptions] = React.useState()
-  const [originalLayout, setOriginalLayout] = React.useState()
-  const [toggleSelection, setToggleSelection] = React.useState('Show Maps')
-  const [toggleColorSelection, setToggleColorSelection] = React.useState('')
-  const [dashboardElements, setDashboardElements] = React.useState()
+  const [dashboard, setDashboard] = React.useState();
+  const [dashboardOptions, setDashboardOptions] = React.useState();
+  const [originalLayout, setOriginalLayout] = React.useState();
+  const [toggleSelection, setToggleSelection] = React.useState("Show Maps");
+  const [toggleColorSelection, setToggleColorSelection] =
+    React.useState("Default");
+  const [dashboardElements, setDashboardElements] = React.useState();
 
   // Function that runs after the "dashboard:run:complete event that initializes the Dashboard Options
-  const initializeDashboardOptions = (event) => {    
+  const initializeDashboardOptions = (event) => {
     // Checks if this is the initial dashboard load
     if (!originalLayout) {
       // Set original elements
       setDashboardElements(event.dashboard.options.elements);
       // Set original layout
-      setOriginalLayout(event.dashboard.options.layouts[0].dashboard_layout_components);
-    }  
+      setOriginalLayout(
+        event.dashboard.options.layouts[0].dashboard_layout_components
+      );
+    }
     // Sets dashboard options for everytime the "dashboard:run:complete" event is triggered
     setDashboardOptions(event.dashboard.options);
-  }
+  };
 
   // Sets the dashboard state
   const setupDashboard = (dashboard) => {
     setDashboard(dashboard);
-  }
+  };
 
   // Function that hides the elements that have map visualizations in the dashboard layout
   const hideMaps = () => {
@@ -40,8 +46,8 @@ const EmbedDashboardLayout = () => {
     // Takes the list from the constant.js to update the options
     newOptions.layouts[0].dashboard_layout_components = newLayoutComponents;
     // Updates the dashboard options with the changes
-    dashboard.setOptions(newOptions)
-  }
+    dashboard.setOptions(newOptions);
+  };
 
   // Function that shows the initial layout
   const showMaps = () => {
@@ -49,95 +55,107 @@ const EmbedDashboardLayout = () => {
     // Takes from the originalLayout state
     newOptions.layouts[0].dashboard_layout_components = originalLayout;
     // Updates the dashboard options with the changes
-    dashboard.setOptions(newOptions)
-  }
+    dashboard.setOptions(newOptions);
+  };
 
-  const useBluePallete = () => {
-    const newOptions = dashboardOptions;
-    // Loops through the bluePallete variable and updates the vis_config for each element
-    bluePallette.map(b => {
-      const keys = Object.keys(b.vis_config);
-      keys.map(k => {
-        const value = b.vis_config[k];
-        newOptions.elements[b.id].vis_config[k] = value;
-      })
-    })
-    // Updates the dashboard options with the changes
-    dashboard.setOptions(newOptions)
-  }
-
-  const handleColorToggle = (color) => {
-    if (color == "") {
-      const newOptions = dashboardOptions;
-      // Returns the elements to the original values
-      newOptions.elements = dashboardElements;
+  const updateDashboardColors = (color) => {
+    const newOptions = { ...dashboardOptions };
+    if (color === "Blue") {
+      // Loops through the bluePallete variable and updates the vis_config for each element
+      bluePallette.forEach((b) => {
+        const keys = Object.keys(b.vis_config);
+        keys.forEach((k) => {
+          const value = b.vis_config[k];
+          newOptions.elements[b.id].vis_config[k] = value;
+        });
+      });
       // Updates the dashboard options with the changes
-      dashboard.setOptions(newOptions)
+      dashboard.setOptions(newOptions);
+    } else if (color === "Default") {
+      // Loops through the defaultPallette variable and updates the vis_config for each element
+      defaultPallette.forEach((b) => {
+        const keys = Object.keys(b.vis_config);
+        keys.forEach((k) => {
+          const value = b.vis_config[k];
+          newOptions.elements[b.id].vis_config[k] = value;
+        });
+      });
+      // Updates the dashboard options with the changes
+      dashboard.setOptions(newOptions);
     }
+    // Update the controlled state for the color picker button toggle
     setToggleColorSelection(color);
-  }
+  };
 
   const makeDashboard = useCallback((el) => {
     if (!el) {
-      return
+      return;
     }
-    el.innerHTML = ''
+    el.innerHTML = "";
     /*
       Step 2 Create your dashboard (or other piece of embedded content) through a simple set of chained methods
     */
-    LookerEmbedSDK.createDashboardWithId("data_block_acs_bigquery::acs_census_overview")
-    
+    LookerEmbedSDK.createDashboardWithId(
+      "data_block_acs_bigquery::acs_census_overview"
+    )
+
       // adds the iframe to the DOM as a child of a specific element
       .appendTo(el)
       // this instructs the SDK to point to the /dashboards-next/ version
       .withNext()
       // this is an event listener for when the dashboard is finished running
-      .on('dashboard:run:complete', initializeDashboardOptions)
+      .on("dashboard:run:complete", initializeDashboardOptions)
       // this line performs the call to the auth service to get the iframe's src='' url, places it in the iframe and the client performs the request to Looker
       .build()
       // this establishes event communication between the iframe and parent page
       .connect()
       // this line sets up the dashboard after the building is complete
-      .then(x => setupDashboard(x))
+      .then((x) => setupDashboard(x))
       // catch various errors which can occur in the process (note: does not catch 404 on content)
       .catch((error) => {
-        console.error('An unexpected error occurred', error)
-      })
-  }, [])
-
+        console.error("An unexpected error occurred", error);
+      });
+  }, []);
 
   return (
     <>
-          <PageTitle text={'Dashboard Layout'} />
-          {/* Elements that contain the toggles for the Layouts and the vis_config color changes*/}
-          <ToggleArea>
-            <ButtonToggle value={toggleSelection} onChange={setToggleSelection}>
-              <ButtonItem onClick={showMaps}>Show Maps</ButtonItem>
-              <ButtonItem onClick={hideMaps}>Hide Maps</ButtonItem>
-            </ButtonToggle>
-            <ButtonToggle style={colorToggleStyle} value={toggleColorSelection} onChange={(color) => handleColorToggle(color)} nullable>
-              <ButtonItem onClick={useBluePallete} value="Blue">
-                <BlueIcon />
-              </ButtonItem>
-            </ButtonToggle>
-          </ToggleArea>
-          { /* Step 0 - we have a simple container, which performs a callback to our makeDashboard function */}
-          <Dashboard ref={makeDashboard}></Dashboard>
+      <PageTitle text={"Dashboard Layout"} />
+      {/* Elements that contain the toggles for the Layouts and the vis_config color changes*/}
+      <ToggleArea>
+        <ButtonToggle value={toggleSelection} onChange={setToggleSelection}>
+          <ButtonItem onClick={showMaps}>Show Maps</ButtonItem>
+          <ButtonItem onClick={hideMaps}>Hide Maps</ButtonItem>
+        </ButtonToggle>
+        <ButtonToggle
+          style={colorToggleStyle}
+          value={toggleColorSelection}
+          onChange={updateDashboardColors}
+          nullable
+        >
+          <ButtonItem value="Default">
+            <GreenIcon />
+          </ButtonItem>
+          <ButtonItem value="Blue">
+            <BlueIcon />
+          </ButtonItem>
+        </ButtonToggle>
+      </ToggleArea>
+      {/* Step 0 - we have a simple container, which performs a callback to our makeDashboard function */}
+      <Dashboard ref={makeDashboard}></Dashboard>
     </>
-  )
-}
+  );
+};
 
-const colorToggleStyle = {  
-  'marginLeft':'auto',
-  'marginRight': '40px'
-}
+const colorToggleStyle = {
+  marginLeft: "auto",
+  marginRight: "40px",
+};
 
-const ToggleArea = styled.div
-`
-  margin-top:20px;
-  margin-left:20px;
-  display:flex
-`
+const ToggleArea = styled.div`
+  margin-top: 20px;
+  margin-left: 20px;
+  display: flex;
+`;
 
 const Dashboard = styled.div`
   width: 100%;
@@ -146,12 +164,19 @@ const Dashboard = styled.div`
     width: 100%;
     height: 100%;
   }
-` 
+`;
 const BlueIcon = styled.span`
-  background-color:rgb(87, 128, 205);
-  width:25px;
-  height:25px;
-  border-radius:50%;
-`
+  background-color: rgb(87, 128, 205);
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+`;
 
-export default EmbedDashboardLayout
+const GreenIcon = styled.span`
+  background-color: #9bc779;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+`;
+
+export default EmbedDashboardLayout;
--- src/components/EmbedDashboardLayout/constant.js
+++ src/components/EmbedDashboardLayout/constant.js
@@ -1,193 +1,278 @@
 export const newLayoutComponents = [
-    {
-        "id": "6dea5eaa44a41c49dff3671b90779174",
-        "dashboard_layout_id": null,
-        "dashboard_element_id": "6dea5eaa44a41c49dff3671b90779174",
-        "row": 0,
-        "column": 5,
-        "width": 14,
-        "height": 3,
-        "deleted": false
-    },
-    {
-        "id": "92f9b48cec1a46b025e4a04d96d7a2f3",
-        "dashboard_layout_id": null,
-        "dashboard_element_id": "92f9b48cec1a46b025e4a04d96d7a2f3",
-        "row": 3,
-        "column": 0,
-        "width": 8,
-        "height": 10,
-        "deleted": false
-    },
-    {
-        "id": "4c4c2ddcc7a34a8798da46341f13d61f",
-        "dashboard_layout_id": null,
-        "dashboard_element_id": "4c4c2ddcc7a34a8798da46341f13d61f",
-        "row": 22,
-        "column": 0,
-        "width": 12,
-        "height": 9,
-        "deleted": false
-    },
-    {
-        "id": "80bc0788621dff5fbee3993a2667d456",
-        "dashboard_layout_id": null,
-        "dashboard_element_id": "80bc0788621dff5fbee3993a2667d456",
-        "row": 3,
-        "column": 8,
-        "width": 8,
-        "height": 10,
-        "deleted": false
-    },
-    {
-        "id": "78a7416ad175bf50857d3e9de55f0610",
-        "dashboard_layout_id": null,
-        "dashboard_element_id": "78a7416ad175bf50857d3e9de55f0610",
-        "row": 3,
-        "column": 16,
-        "width": 8,
-        "height": 10,
-        "deleted": false
-    },
-    {
-        "id": "71033d571917797d45927d87b12ac354",
-        "dashboard_layout_id": null,
-        "dashboard_element_id": "71033d571917797d45927d87b12ac354",
-        "row": 31,
-        "column": 12,
-        "width": 12,
-        "height": 9,
-        "deleted": false
-    },
-    {
-        "id": "730e892bb4f142542673492851b6c8cd",
-        "dashboard_layout_id": null,
-        "dashboard_element_id": "730e892bb4f142542673492851b6c8cd",
-        "row": 22,
-        "column": 12,
-        "width": 12,
-        "height": 9,
-        "deleted": false
-    },
-    {
-        "id": "892494557fbcfdcbc0aa1c4aeeb753b3",
-        "dashboard_layout_id": null,
-        "dashboard_element_id": "892494557fbcfdcbc0aa1c4aeeb753b3",
-        "row": 13,
-        "column": 0,
-        "width": 12,
-        "height": 9,
-        "deleted": false
-    }
-]
+  {
+    id: "6dea5eaa44a41c49dff3671b90779174",
+    dashboard_layout_id: null,
+    dashboard_element_id: "6dea5eaa44a41c49dff3671b90779174",
+    row: 0,
+    column: 5,
+    width: 14,
+    height: 3,
+    deleted: false,
+  },
+  {
+    id: "92f9b48cec1a46b025e4a04d96d7a2f3",
+    dashboard_layout_id: null,
+    dashboard_element_id: "92f9b48cec1a46b025e4a04d96d7a2f3",
+    row: 3,
+    column: 0,
+    width: 8,
+    height: 10,
+    deleted: false,
+  },
+  {
+    id: "4c4c2ddcc7a34a8798da46341f13d61f",
+    dashboard_layout_id: null,
+    dashboard_element_id: "4c4c2ddcc7a34a8798da46341f13d61f",
+    row: 22,
+    column: 0,
+    width: 12,
+    height: 9,
+    deleted: false,
+  },
+  {
+    id: "80bc0788621dff5fbee3993a2667d456",
+    dashboard_layout_id: null,
+    dashboard_element_id: "80bc0788621dff5fbee3993a2667d456",
+    row: 3,
+    column: 8,
+    width: 8,
+    height: 10,
+    deleted: false,
+  },
+  {
+    id: "78a7416ad175bf50857d3e9de55f0610",
+    dashboard_layout_id: null,
+    dashboard_element_id: "78a7416ad175bf50857d3e9de55f0610",
+    row: 3,
+    column: 16,
+    width: 8,
+    height: 10,
+    deleted: false,
+  },
+  {
+    id: "71033d571917797d45927d87b12ac354",
+    dashboard_layout_id: null,
+    dashboard_element_id: "71033d571917797d45927d87b12ac354",
+    row: 31,
+    column: 12,
+    width: 12,
+    height: 9,
+    deleted: false,
+  },
+  {
+    id: "730e892bb4f142542673492851b6c8cd",
+    dashboard_layout_id: null,
+    dashboard_element_id: "730e892bb4f142542673492851b6c8cd",
+    row: 22,
+    column: 12,
+    width: 12,
+    height: 9,
+    deleted: false,
+  },
+  {
+    id: "892494557fbcfdcbc0aa1c4aeeb753b3",
+    dashboard_layout_id: null,
+    dashboard_element_id: "892494557fbcfdcbc0aa1c4aeeb753b3",
+    row: 13,
+    column: 0,
+    width: 12,
+    height: 9,
+    deleted: false,
+  },
+];
 
 export const bluePallette = [
-    {
-        id:'6dea5eaa44a41c49dff3671b90779174',
-        vis_config:{
-            custom_color:'#4276BE'
-        }
+  {
+    id: "6dea5eaa44a41c49dff3671b90779174",
+    vis_config: {
+      custom_color: "#4276BE",
     },
-    {
-        id:'92f9b48cec1a46b025e4a04d96d7a2f3',
-        vis_config:{
-            series_colors: {
-                "blockgroup.female_pop": "#0a1159",
-                "blockgroup.male_pop": "#4276BE"
-            }
-        }
+  },
+  {
+    id: "92f9b48cec1a46b025e4a04d96d7a2f3",
+    vis_config: {
+      series_colors: {
+        "blockgroup.female_pop": "#0a1159",
+        "blockgroup.male_pop": "#4276BE",
+      },
     },
-    {
-        id:'80bc0788621dff5fbee3993a2667d456',
-        vis_config:{
-            series_colors:{
-                "blockgroup.white_pop": "#0a1159",
-                "blockgroup.hispanic_pop": "#4276BE",
-                "blockgroup.black_pop": "#5188bf",
-                "blockgroup.asian_pop": "#7bb2de",
-                "blockgroup.amerindian_pop": "#94c8eb",
-                "blockgroup.other_race_pop": "#b3e2f7"
-            }
+  },
+  {
+    id: "80bc0788621dff5fbee3993a2667d456",
+    vis_config: {
+      series_colors: {
+        "blockgroup.white_pop": "#0a1159",
+        "blockgroup.hispanic_pop": "#4276BE",
+        "blockgroup.black_pop": "#5188bf",
+        "blockgroup.asian_pop": "#7bb2de",
+        "blockgroup.amerindian_pop": "#94c8eb",
+        "blockgroup.other_race_pop": "#b3e2f7",
+      },
+    },
+  },
+  {
+    id: "78a7416ad175bf50857d3e9de55f0610",
+    vis_config: {
+      series_colors: {
+        "blockgroup.masters_degree": "#0a1159",
+        "blockgroup.bachelors_degree": "#4276BE",
+        "blockgroup.associates_degree": "#5188bf",
+        "blockgroup.one_year_more_college": "#7bb2de",
+        "blockgroup.less_one_year_college": "#94c8eb",
+        "blockgroup.high_school_diploma": "#b3e2f7",
+      },
+    },
+  },
+  {
+    id: "892494557fbcfdcbc0aa1c4aeeb753b3",
+    vis_config: {
+      series_colors: {
+        "state.state_percent_income_spent_on_rent": "#0a1159",
+        "state.state_median_income": "#7bb2de",
+      },
+    },
+  },
+  {
+    id: "4c4c2ddcc7a34a8798da46341f13d61f",
+    vis_config: {
+      series_colors: {
+        "state.state_median_income": "#0a1159",
+        "state.state_income_per_capita": "#5188bf",
+      },
+    },
+  },
+  {
+    id: "71033d571917797d45927d87b12ac354",
+    vis_config: {
+      series_colors: {
+        "blockgroup.employed_pop": "#5188bf",
+        "blockgroup.unemployed_pop": "#0a1159",
+      },
+    },
+  },
+  {
+    id: "68de526333bf14aff72cca35d17c2157",
+    vis_config: {
+      map_value_colors: ["#0a1159", "#b3e2f7"],
+    },
+  },
+  {
+    id: "92bfc3c762bcea17abf1030173d64bb6",
+    vis_config: {
+      map_value_colors: ["#0a1159", "#b3e2f7"],
+    },
+  },
+  {
+    id: "a07c95e32a2b16dd854252022f4b4400",
+    vis_config: {
+      map_value_colors: ["#0a1159", "#b3e2f7"],
+    },
+  },
+  {
+    id: "93d6ab1af0f91075ae43f3ed8e20d125",
+    vis_config: {
+      map_value_colors: ["#0a1159", "#b3e2f7"],
+    },
+  },
+  {
+    id: "e502982721706a690da51073e80f278d",
+    vis_config: {
+      map_value_colors: ["#0a1159", "#b3e2f7"],
+    },
+  },
+];
 
-        }
+export const defaultPallette = [
+  {
+    id: "68de526333bf14aff72cca35d17c2157",
+    vis_config: {
+      map_value_colors: [],
     },
-    {
-        id:'78a7416ad175bf50857d3e9de55f0610',
-        vis_config:{
-            series_colors:{
-                "blockgroup.masters_degree": "#0a1159",
-                "blockgroup.bachelors_degree": "#4276BE",
-                "blockgroup.associates_degree": "#5188bf",
-                "blockgroup.one_year_more_college": "#7bb2de",
-                "blockgroup.less_one_year_college": "#94c8eb",
-                "blockgroup.high_school_diploma": "#b3e2f7"
-            }       
-
-        }
+  },
+  {
+    id: "6dea5eaa44a41c49dff3671b90779174",
+    vis_config: {},
+  },
+  {
+    id: "92f9b48cec1a46b025e4a04d96d7a2f3",
+    vis_config: {
+      series_colors: {},
     },
-    {
-        id:'892494557fbcfdcbc0aa1c4aeeb753b3',
-        vis_config:{
-            series_colors:{
-                "state.state_percent_income_spent_on_rent": "#0a1159",
-                "state.state_median_income": "#7bb2de"
-            }            
-
-        }
+  },
+  {
+    id: "4c4c2ddcc7a34a8798da46341f13d61f",
+    vis_config: {
+      series_colors: {
+        "state.state_median_income": "#E57947",
+      },
     },
-    {
-        id:'4c4c2ddcc7a34a8798da46341f13d61f',
-        vis_config:{
-            series_colors:{
-                "state.state_median_income": "#0a1159",
-                "state.state_income_per_capita": "#5188bf"
-            }            
-
-        }
+  },
+  {
+    id: "92bfc3c762bcea17abf1030173d64bb6",
+    vis_config: {
+      series_colors: {
+        "state.state_median_rent": "#E57947",
+      },
+      map_value_colors: [],
     },
-    {
-        id:'71033d571917797d45927d87b12ac354',
-        vis_config:{
-            series_colors:{
-                "blockgroup.employed_pop": "#5188bf",
-                "blockgroup.unemployed_pop": "#0a1159"
-            }
-
-        }
+  },
+  {
+    id: "80bc0788621dff5fbee3993a2667d456",
+    vis_config: {
+      series_colors: {
+        "state.state_median_rent": "#E57947",
+      },
     },
-    {
-        id:'68de526333bf14aff72cca35d17c2157',
-        vis_config:{
-            map_value_colors: ["#0a1159", "#b3e2f7"]        
-
-        }
+  },
+  {
+    id: "78a7416ad175bf50857d3e9de55f0610",
+    vis_config: {
+      series_colors: {},
     },
-    {
-        id:'92bfc3c762bcea17abf1030173d64bb6',
-        vis_config:{
-            map_value_colors: ["#0a1159", "#b3e2f7"]        
-
-        }
+  },
+  {
+    id: "71033d571917797d45927d87b12ac354",
+    vis_config: {
+      series_colors: {
+        "blockgroup.employed_pop": "#FFD95F",
+        "blockgroup.unemployed_pop": "#E57947",
+      },
     },
-    {
-        id:'a07c95e32a2b16dd854252022f4b4400',
-        vis_config:{
-            map_value_colors: ["#0a1159", "#b3e2f7"]        
-
-        }
+  },
+  {
+    id: "730e892bb4f142542673492851b6c8cd",
+    vis_config: {
+      series_colors: {
+        "blockgroup.employed_pop": "#FFD95F",
+        "blockgroup.unemployed_pop": "#E57947",
+      },
     },
-    {
-        id:'93d6ab1af0f91075ae43f3ed8e20d125',
-        vis_config:{
-            map_value_colors: ["#0a1159", "#b3e2f7"]        
-
-        }
+  },
+  {
+    id: "892494557fbcfdcbc0aa1c4aeeb753b3",
+    vis_config: {
+      series_colors: {
+        "state.state_percent_income_spent_on_rent": "#E57947",
+        "state.state_median_income": "#B42F37",
+      },
     },
-    {
-        id:'e502982721706a690da51073e80f278d',
-        vis_config:{
-            map_value_colors: ["#0a1159", "#b3e2f7"]        
-
-        }
-    }
-]
+  },
+  {
+    id: "a07c95e32a2b16dd854252022f4b4400",
+    vis_config: {
+      map_value_colors: [],
+    },
+  },
+  {
+    id: "e502982721706a690da51073e80f278d",
+    vis_config: {
+      map_value_colors: [],
+    },
+  },
+  {
+    id: "93d6ab1af0f91075ae43f3ed8e20d125",
+    vis_config: {
+      map_value_colors: [],
+    },
+  },
+];
